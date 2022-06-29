21f688c5e5eb5724eb43533035a85b6dca18a6cc
Ricardo-DSS <ricardo.350.rs@gmail.com>
2022-06-29 16:27:43 -0300

    rewrite both methods. Now it can open a yt video

--- control.py
+++ control.py
@@ -4,11 +4,24 @@ def menu():
     print('Você deseja ver algo com: ' +
           '\n1. Ação' +
           '\n2. Aventura' +
-          '\n3. Carros' +
           '\n4. Comédia' +
           '\n5. Vanguarda' +
-          '\n6. Demônios' +
-          '\n7. Mistério')
-    opcao = int(input())
-    mal.genero(opcao)
+          '\n7. Mistério' +
+          '\n8. Drama' +
+          '\n10. Fantasia' +
+          '\n22. Romance' +
+          '\n24. Ficção Científica' +
+          '\n27. Shounen' +
+          '\n30. Esportes' +
+          '\n36. Slice of life' +
+          '\n37. Sobrenatural')
+    opcaoGenero = int(input())
+    print('Você prefere algo: ' +
+          '\n1. Muito popular' +
+          '\n2. Popular' +
+          '\n3. Conhecido' +
+          '\n4. Pouco conhecido' +
+          '\n5. Praticamente desconhecido')
+    opcaoFama = int(input())
+    mal.instancia(opcaoGenero, opcaoFama)
 
--- mal.py
+++ mal.py
@@ -1,39 +1,62 @@
-import pandas as pd
+
+global genre
+global pagina
+
 import json
 import random
-from googletrans import Translator # pip3 install googletrans==3.1.0a0
-from jikanpy import Jikan # pip install jikanpy no terminal
+from googletrans import Translator  #pip3 install googletrans==3.1.0a0
+from jikanpy import Jikan  #pip install jikanpy no terminal
+import pywhatkit  #pip3 install pywhatkit
 
 tradutor = Translator()
 jikan = Jikan()
 
-def procurar(iden, sinopse):
-    dadosApi = jikan.search('anime', iden, page=1)
+def procurar(identificador):
+    openings = []
+
+    dadosApi = jikan.anime(identificador, page=1)
     # print(json.dumps(dadosApi, sort_keys=False, indent=4))
 
-    dados = dadosApi["results"]
-    # print(json.dumps(dados, sort_keys=False, indent=4))
+    # A condição abaixo evita o retorno de resultados diferentes como filmes, especiais etc
+    if dadosApi["type"] == "TV":
+        titulo = dadosApi["title"]
+        url = dadosApi["image_url"]
+        ep = dadosApi["episodes"]
+        sinopse = str(dadosApi["synopsis"]).replace('[Written by MAL Rewrite]', ' ')
+        sinopse = tradutor.translate(sinopse, dest="pt")
+        openings = dadosApi["opening_themes"]
 
-    df = pd.DataFrame.from_dict(dados)
-    #print(df)
-    analise = df.index[df['title'] == iden].tolist()
-    linha = analise[0]
-    print('Anime: ', iden)
-    print('Episódios: ', str(df.iat[linha, 7]))
-    sinopse = tradutor.translate(sinopse, dest="pt")
-    print('Sinopse: ', sinopse.text)
-    print('imagem: ', str(df.iat[linha, 2]))
+        print('Anime: {}'.format(titulo))
+        print('Episódios: {}'.format(ep))
+        print('Sinopse: {}'.format(sinopse.text))
+        print('Imagem: {}'.format(url))
+        print('Opening: {}'.format(openings[0]))
 
-def genero(opcao):
-    dadosApi = jikan.genre('anime', genre_id=opcao)
+        if not openings:
+            print('Não há abertura')
+        else:
+            abertura = titulo + ' opening'
+            pywhatkit.playonyt(abertura)
+    else:
+        genero(genre, pagina)
+
+def genero(genre, pagina):
+    dadosApi = jikan.genre("anime", genre_id=genre, page=pagina)
     # print(json.dumps(dadosApi, sort_keys=False, indent=4))
 
     dados = dadosApi["anime"]
-    print(json.dumps(dados, sort_keys=False, indent=4))
+    # print(json.dumps(dados, sort_keys=False, indent=4))
 
     posicaoAnime = random.randint(0, 99)
     anime = dados[posicaoAnime]
-    iden = str(anime["title"])
-    sinopse = str(anime["synopsis"]).replace('\r\n\r\n[Written by MAL Rewrite]', ' ')
+    identificador = int(anime["mal_id"])
     # print(json.dumps(anime, sort_keys=False, indent=4))
-    procurar(iden, sinopse)
+    procurar(identificador)
+
+def instancia(opcaoGenero, opcaoFama):
+    '''Pode ser necessário usar o método genero mais de uma vez
+    portanto na primeira vez irei armazenar as variaveis da control
+    nas variáveis globais abaixo'''
+    genre = opcaoGenero
+    pagina = opcaoFama
+    genero(genre, pagina)
